e2e561be7423f4a4962cfd023edf1a551949488b
Trinity Kim Orta <trinitykimorta@trinitys-air-2.ch_wlan_data.tamuk.edu>
2026-03-13 10:53:35 -0500

    force replace auth resource

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,19 +1,5 @@
-import { defineAuth } from "@aws-amplify/backend";
-import { postConfirmation } from "./post-confirmation/resource";
+import { defineAuth } from '@aws-amplify/backend';
 
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
-  triggers: {
-    postConfirmation,
-  },
-});import { defineAuth } from '@aws-amplify/backend';
-
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
